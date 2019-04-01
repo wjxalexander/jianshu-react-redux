844d85c730ee4bdd4e8a8d618e40cf5c7642c50a
wjxalexander <wjxalexander@hotmail.com>
2019-04-01 21:39:45 +0800

    prepare redux

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,123 +1,123 @@
-import styled from "styled-components"
-import logo from "../../assets/images/logo.png"
-export const HeaderWrapper = styled.div `
-   height: 56px;
-   border-bottom: 1px solid #f0f0f0;
- `
+import styled from "styled-components";
+import logo from "../../assets/images/logo.png";
+export const HeaderWrapper = styled.div`
+  height: 56px;
+  border-bottom: 1px solid #f0f0f0;
+`;
 export const Logo = styled.a.attrs({
   href: "/"
-})
-`
-   display:inline-block;
-   position:absolute;
-   top:0;
-   left:0;
-   width: 100px;
-   height: 56px;
-   background: url(${logo});
-   background-size:contain;
-`
-export const Nav = styled.div `
-   width: 960px;
-   height: 100%;
-   margin: 0 auto;
-    &.clearfix:after{
-      content:"";
-      display:block;
-      height:0;
-      clear:both;
-      visibility:hidden}
-`
+})`
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100px;
+  height: 56px;
+  background: url(${logo});
+  background-size: contain;
+`;
+export const Nav = styled.div`
+  width: 960px;
+  height: 100%;
+  margin: 0 auto;
+  &.clearfix:after {
+    content: "";
+    display: block;
+    height: 0;
+    clear: both;
+    visibility: hidden;
+  }
+`;
 
-export const NavItem = styled.div `
-   line-height:56px;
-   padding: 0 15px;
-   font-size: 17px;
-   color: #333;
-   &.left{
-     float:left
-   }
-   &.right{
-     float:right;
-     color: #969696;
-   }
-   &.active{
-     color: #ea6f5a;
-   }
-`
+export const NavItem = styled.div`
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  color: #333;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+`;
 export const NavSearch = styled.input.attrs({
-  placeholder: 'search'
-})` 
-  &.focused{
-    width:240px;
+  placeholder: "search"
+})`
+  &.focused {
+    width: 240px;
   }
   &:focus {
-    outline:none;
+    outline: none;
   }
   padding: 0 30px 0 20px;
   margin: 9px 0 0 20px;
   width: 160px;
   height: 38px;
-  border:none;
-  border-radius:19px;
+  border: none;
+  border-radius: 19px;
   background: #eee;
   font-size: 14px;
   color: #666;
-  &::placeholder{
-    color:#999;
+  &::placeholder {
+    color: #999;
   }
-`
-export const Addition = styled.div `
-  position:absolute;
-  right:0;
+  /* css transition */
+  &.slide-enter {
+    width: 160px;
+    transition: all 0.5s ease-out;
+  }
+  &.slide-enter-active {
+    width: 240px;
+  }
+  &.slide-exit {
+    transition: all 0.5s ease-out;
+  }
+  &.slide-exit-active {
+    width: 160px;
+  }
+`;
+export const Addition = styled.div`
+  position: absolute;
+  right: 0;
   top: 0;
-  height:56px;
- `
-export const Button = styled.div `
-   font-size: 17px;
-  float:right;
-  margin:9px 20px 0 0;
+  height: 56px;
+`;
+export const Button = styled.div`
+  font-size: 17px;
+  float: right;
+  margin: 9px 20px 0 0;
   padding: 0 20px;
   line-height: 38px;
   border-radius: 19px;
   border: 1px solid #ec6149;
-  &.register{
-    color: #ec6149
+  &.register {
+    color: #ec6149;
   }
-  &.writing{
+  &.writing {
     color: #fff;
     background: #ec6149;
   }
 `;
 export const SearchWrapper = styled.div`
-  float:left;
-  position:relative;
-   .iconfont{
+  float: left;
+  position: relative;
+  .iconfont {
     display: inline-block;
-    position:absolute;
-    right:5px;
+    position: absolute;
+    right: 5px;
     bottom: 5px;
-    width:30px;
+    width: 30px;
     border-radius: 15px;
-    line-height:30px;
-    text-align:center;
-    &.focused{
-       background:#777;
-       color:#fff;
+    line-height: 30px;
+    text-align: center;
+    &.focused {
+      background: #777;
+      color: #fff;
     }
   }
-  .slide-enter{
-     width:160px;
-     transition: all 0.5s ease-out;
-  }
-  .slide-enter-active{
-      width:240px;
-  }
-  .slide-exit{
-    transition: all 0.5s ease-out;
-  }
-  .slide-exit-active{
-    width: 160px;
-  }
-
 `;
